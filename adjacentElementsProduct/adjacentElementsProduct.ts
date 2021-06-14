8bbfaec300186d2ee313bf1d6bdfcc74ddf118bd
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-14 19:23:58 +0700

    Solve adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,12 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
+  let result = inputArray[0] * inputArray[1];
+  for (let index = 1; index < inputArray.length - 1; index++) {
+    const product = inputArray[index] * inputArray[index + 1];
 
+    result = result > product ? result : product;
+  }
+
+  return result;
 }
 
 //console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
