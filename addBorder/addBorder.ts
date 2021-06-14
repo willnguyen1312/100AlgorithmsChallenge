256d7d43d4f498565591a91c9e653ed16cadfaed
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-14 19:04:28 +0700

    Solve add addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,14 @@
 export function addBorder(picture: string[]): string[] {
+  const lengthOfWall = picture[0].length + 2;
+  const wall = "*".repeat(lengthOfWall);
+  picture.push(wall);
+  picture.unshift(wall);
 
+  for (let index = 1; index < picture.length - 1; index++) {
+    picture[index] = "*" + picture[index] + "*";
+  }
+
+  return picture;
 }
 
 // console.log(addBorder(["abc", "ded"]));
