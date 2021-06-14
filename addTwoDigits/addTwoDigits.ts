710dca88f1e3682b358b94f4e2ae75bfd778db89
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-14 19:10:23 +0700

    Solve addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,7 @@
-export function addTwoDigits(n: any): number {
+export function addTwoDigits(n: number): number {
+  const arrNumb = n.toString().toString();
 
+  return arrNumb.split("").reduce((a, b) => +a + +b, 0);
 }
 
 // console.log(addTwoDigits(29));
