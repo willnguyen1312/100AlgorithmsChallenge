1a965663c57ee32d9c22b532fad0cb598d55bbfb
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 11:00:36 +0700

    Solve almostIncreasingSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,17 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+  let numberOfRemove = 0;
 
+  for (let index = 0; index < sequence.length - 1; index++) {
+    const element = sequence[index];
+    const nextElement = sequence[index + 1];
+
+    if (nextElement < element) {
+      numberOfRemove += 1;
+    }
+  }
+
+  return numberOfRemove <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+// console.log(almostIncreasingSequence([1, 3, 2, 1]))
+// console.log(almostIncreasingSequence([1, 3, 2]))
