7c66904e5ae6499972073f9bbe39d35c08264cea
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 12:01:05 +0700

    Solve alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,27 @@
 export function alphabetSubsequence(s: string): boolean {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const length = alphabet.length;
+  const lookup: Record<number, string> = alphabet
+    .split("")
+    .reduce((acc, curr, index) => {
+      acc[index] = curr;
+      acc[curr] = index;
+      return acc;
+    }, {});
 
+  for (let index = 0; index < s.length - 1; index++) {
+    const element = s[index];
+
+    const nextElement = s[index + 1];
+
+    if (lookup[nextElement] <= lookup[element]) return false;
+  }
+
+  return true;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+// console.log(alphabetSubsequence('zab'))
+// console.log(alphabetSubsequence('effg'))
+// console.log(alphabetSubsequence('cdce'))
+// console.log(alphabetSubsequence('ace'))
+// console.log(alphabetSubsequence('bxz'))
