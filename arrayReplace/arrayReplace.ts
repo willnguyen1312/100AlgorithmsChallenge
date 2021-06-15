16802af7f4c9228ba89845061d37d852b5e2927b
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 17:47:55 +0700

    Solve arrayReplace

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,14 @@
-export function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-
+export function arrayReplace(
+  inputArray: number[],
+  elemToReplace: number,
+  substitutionElem: number
+): number[] {
+  return inputArray.map((value) => {
+    if (value === elemToReplace) {
+      return substitutionElem;
+    }
+    return value;
+  });
 }
 
 // console.log(arrayReplace([1, 2, 1], 1, 3));
