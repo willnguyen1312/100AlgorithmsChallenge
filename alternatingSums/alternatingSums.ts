4f45ca5985148dc69615b72c8a90c61d10d7015a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 12:07:58 +0700

    Solve alternatingSums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,18 @@
 export function alternatingSums(a: number[]): number[] {
+  let team1Sum = 0;
+  let team2Sum = 0;
 
+  for (let index = 0; index < a.length; index++) {
+    const element = a[index];
+    const isTeam1 = index % 2 === 0;
+    if (isTeam1) {
+      team1Sum += element;
+    } else {
+      team2Sum += element;
+    }
+  }
+
+  return [team1Sum, team2Sum];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+// console.log(alternatingSums([50, 60, 60, 45, 70]));
