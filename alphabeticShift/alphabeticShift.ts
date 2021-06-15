73b4c7b14ba2c6ac6ffc4687956c24880978621a
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 11:23:30 +0700

    Solve alphabeticShift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,23 @@
 export function alphabeticShift(inputString: string): string {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const length = alphabet.length;
+  const lookup: Record<number, string> = alphabet
+    .split("")
+    .reduce((acc, curr, index) => {
+      acc[index] = curr;
+      acc[curr] = index;
+      return acc;
+    }, {});
 
+  const strArr = inputString.split("");
+
+  for (let index = 0; index < strArr.length; index++) {
+    const element = strArr[index];
+    let eleIndex = lookup[element];
+    strArr[index] = lookup[(eleIndex + 1) % length];
+  }
+
+  return strArr.join("");
 }
 
-console.log(alphabeticShift('crazy'));
+// console.log(alphabeticShift('crazy'));
