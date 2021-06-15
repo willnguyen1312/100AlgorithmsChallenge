0fe8adcf5af8345c0aead74fe8c578579faea78e
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 16:58:56 +0700

    Solve areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,10 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+export function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  return Math.min(yourLeft, yourRight) === Math.min(friendsLeft, friendsRight);
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
