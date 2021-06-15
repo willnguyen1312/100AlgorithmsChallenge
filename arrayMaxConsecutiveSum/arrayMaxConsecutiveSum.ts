842b64f40cafef8f258070a88c133fa012fd1279
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 17:34:58 +0700

    Solve arrayMaxConsecutiveSum

--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,26 @@
-export function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+export function arrayMaxConsecutiveSum(
+  inputArray: number[],
+  k: number
+): number {
+  let sum = 0;
+  let max = 0;
 
+  for (let index = 0; index < k; index++) {
+    sum += inputArray[index];
+  }
+
+  max = sum;
+
+  for (let index = k; index < inputArray.length; index++) {
+    sum += inputArray[index];
+    sum -= inputArray[index - k];
+
+    if (sum > max) {
+      max = sum;
+    }
+  }
+
+  return max;
 }
 
 // console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
