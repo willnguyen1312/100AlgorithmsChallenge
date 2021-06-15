65b0bf5ed8fc04ceda69945498167e7dbe006a19
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 10:57:27 +0700

    Solve allLongestStrings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,22 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+  const lengthLookup: Record<number, string[]> = {};
 
+  let maxLength = inputArray[0].length;
+  lengthLookup[maxLength] = [inputArray[0]];
+
+  for (let index = 1; index < inputArray.length; index++) {
+    const element = inputArray[index];
+    const length = element.length;
+
+    maxLength = maxLength > length ? maxLength : length;
+    if (lengthLookup[length]) {
+      lengthLookup[length].push(element);
+    } else {
+      lengthLookup[length] = [element];
+    }
+  }
+
+  return lengthLookup[maxLength];
 }
 
-console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+// console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
