4ba0b4efee1a84fabc409a604f2b8e30fbfe97a1
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 17:15:11 +0700

    Solve arrayChange

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,18 @@
 export function arrayChange(inputArray: number[]): number {
+  let result = 0;
 
+  for (let index = 1; index < inputArray.length; index++) {
+    const element = inputArray[index];
+    const prevElement = inputArray[index - 1];
+
+    if (element <= prevElement) {
+      const increment = prevElement - element + 1;
+      result = result + increment;
+      inputArray[index] = element + increment;
+    }
+  }
+
+  return result;
 }
 
 // console.log(arrayChange([1, 1, 1]));
