b3872252d03167e71081b5a91c00fa512586114d
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 17:54:17 +0700

    Solve avoidObstacles

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,12 @@
 export function avoidObstacles(inputArray: number[]): number {
+  const sortedArr = inputArray.sort((a, b) => a - b);
+  const largest = sortedArr[sortedArr.length - 1];
 
+  for (let num = 1; num <= largest + 1; num++) {
+    if (sortedArr.every((value) => value % num !== 0)) {
+      return num;
+    }
+  }
 }
 
 // console.log(avoidObstacles([5, 3, 6, 7, 9]));
