11cd3a7fd977a64c287aa7c96f6d9a307bdc8ae9
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 17:45:15 +0700

    Solve arrayPreviousLess

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,19 @@
 export function arrayPreviousLess(items: number[]): number[] {
+  let smallest = items[0];
+  const result: number[] = [];
 
+  for (let index = 0; index < items.length; index++) {
+    const element = items[index];
+    if (element <= smallest) {
+      result.push(-1);
+    } else {
+      result.push(smallest);
+    }
+
+    smallest = element;
+  }
+
+  return result;
 }
 
 // console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
