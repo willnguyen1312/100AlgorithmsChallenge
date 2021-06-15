2e5d9659000dd5267edbdf6b31a76fa621fef226
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-06-15 17:38:22 +0700

    Solve arrayMaximalAdjacentDifference

--- arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -1,5 +1,15 @@
 export function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let result = Math.abs(inputArray[0] - inputArray[1]);
 
+  for (let index = 1; index < inputArray.length - 1; index++) {
+    const element = inputArray[index];
+    const nextElement = inputArray[index + 1];
+    const currentDiff = Math.abs(element - nextElement);
+
+    result = result > currentDiff ? result : currentDiff;
+  }
+
+  return result;
 }
 
 // console.log(arrayMaximalAdjacentDifference([2, 4, 1, 0]));
